c4143252bbfe7abcaec591e40b8721d7f472fbef
Evan Brown <evanbrown@google.com>
2017-12-19 19:36:56 -0800

    Update gitignore

--- src/main/java/com/google/jenkins/plugins/computeengine/ComputeEngineCommandLauncher.java
+++
@@ -1,9 +0,0 @@
-package com.google.jenkins.plugins.computeengine;
-
-import hudson.slaves.CommandLauncher;
-
-public class ComputeEngineCommandLauncher extends CommandLauncher {
-  public ComputeEngineCommandLauncher(String command) {
-    super(command);
-  }
-}
