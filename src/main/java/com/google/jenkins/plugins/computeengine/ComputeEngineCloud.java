99718d91c9991bd72e589015559ee076bc89acd1
Evan Brown <evanbrown@google.com>
2017-12-19 19:37:23 -0800

    Rudimentary support for config forms

--- src/main/java/com/google/jenkins/plugins/computeengine/ComputeEngineCloud.java
+++ src/main/java/com/google/jenkins/plugins/computeengine/ComputeEngineCloud.java
@@ -2,16 +2,33 @@ package com.google.jenkins.plugins.computeengine;
 
 import hudson.Extension;
 import hudson.model.Label;
+import hudson.slaves.AbstractCloudImpl;
 import hudson.slaves.NodeProvisioner.PlannedNode;
 import hudson.slaves.Cloud;
 import hudson.model.Descriptor;
 import javax.annotation.Nonnull;
 import java.util.Collection;
+import org.kohsuke.stapler.DataBoundConstructor;
 
-public class ComputeEngineCloud extends Cloud {
+public class ComputeEngineCloud extends AbstractCloudImpl {
 
-  public ComputeEngineCloud(String name) {
-    super(name);
+
+
+  /**
+   * The Google Cloud Platform project ID for this cloud instance
+   */
+  private String projectId;
+
+  /**
+   * The Google Service Account key or name as specified in the Jenkins credentials store
+   */
+  private String credentialsId;
+
+  @DataBoundConstructor
+  public ComputeEngineCloud(String name, String projectId, String credentialsId, String instanceCapStr) {
+    super(name, instanceCapStr);
+    setCredentialsId(credentialsId);
+    setProjectId(projectId);
   }
 
   @Override
@@ -32,4 +49,21 @@ public class ComputeEngineCloud extends Cloud {
       return Messages.ComputeEngineCloud_DisplayName();
     }
   }
+
+  public String getProjectId() {
+    return projectId;
+  }
+
+  public void setProjectId(String projectId) {
+    this.projectId = projectId;
+  }
+
+  public String getCredentialsId() {
+    return credentialsId;
+  }
+
+  public void setCredentialsId(String credentialsId) {
+    this.credentialsId = credentialsId;
+  }
+
 }
