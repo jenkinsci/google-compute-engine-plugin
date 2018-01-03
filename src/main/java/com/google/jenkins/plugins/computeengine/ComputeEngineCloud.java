df24274bd10dbc5af4bcb73ea640393c2d767e66
Evan Brown <evanbrown@google.com>
2018-01-02 19:02:07 -0800

    Basic credentials handling

--- src/main/java/com/google/jenkins/plugins/computeengine/ComputeEngineCloud.java
+++ src/main/java/com/google/jenkins/plugins/computeengine/ComputeEngineCloud.java
@@ -1,69 +1,122 @@
 package com.google.jenkins.plugins.computeengine;
 
+import com.cloudbees.plugins.credentials.CredentialsMatchers;
+import com.cloudbees.plugins.credentials.CredentialsProvider;
+import com.cloudbees.plugins.credentials.common.StandardCredentials;
+import com.cloudbees.plugins.credentials.common.StandardListBoxModel;
+import com.cloudbees.plugins.credentials.domains.DomainRequirement;
+import com.google.jenkins.plugins.computeengine.client.ClientFactory;
+import com.google.jenkins.plugins.credentials.oauth.GoogleOAuth2Credentials;
 import hudson.Extension;
+import hudson.model.Item;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import static java.util.logging.Level.INFO;
+
 import hudson.model.Label;
+import hudson.security.ACL;
 import hudson.slaves.AbstractCloudImpl;
 import hudson.slaves.NodeProvisioner.PlannedNode;
 import hudson.slaves.Cloud;
 import hudson.model.Descriptor;
+
 import javax.annotation.Nonnull;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+
+import hudson.util.FormValidation;
+import hudson.util.ListBoxModel;
+import jenkins.model.Jenkins;
+import org.kohsuke.stapler.AncestorInPath;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.QueryParameter;
 
 public class ComputeEngineCloud extends AbstractCloudImpl {
+    private static final Logger logger =
+            Logger.getLogger(ComputeEngineCloud.class.getName());
 
 
+    /**
+     * The Google Cloud Platform project ID for this cloud instance
+     */
+    private String projectId;
 
-  /**
-   * The Google Cloud Platform project ID for this cloud instance
-   */
-  private String projectId;
+    /**
+     * The Google Service Account key or name as specified in the Jenkins credentials store
+     */
+    private String credentialsId;
 
-  /**
-   * The Google Service Account key or name as specified in the Jenkins credentials store
-   */
-  private String credentialsId;
-
-  @DataBoundConstructor
-  public ComputeEngineCloud(String name, String projectId, String credentialsId, String instanceCapStr) {
-    super(name, instanceCapStr);
-    setCredentialsId(credentialsId);
-    setProjectId(projectId);
-  }
-
-  @Override
-  public Collection<PlannedNode> provision(Label label, int excessWorkload) {
-    return null;
-  }
-
-  @Override
-  public boolean canProvision(Label label) {
-    return true;
-  }
-
-  @Extension
-  public static class GoogleCloudDescriptor extends Descriptor<Cloud> {
-    @Nonnull
-    @Override
-    public String getDisplayName() {
-      return Messages.ComputeEngineCloud_DisplayName();
+    @DataBoundConstructor
+    public ComputeEngineCloud(String name, String projectId, String credentialsId, String instanceCapStr) {
+        super(name, instanceCapStr);
+        setCredentialsId(credentialsId);
+        setProjectId(projectId);
     }
-  }
 
-  public String getProjectId() {
-    return projectId;
-  }
+    @Override
+    public Collection<PlannedNode> provision(Label label, int excessWorkload) {
+        return null;
+    }
 
-  public void setProjectId(String projectId) {
-    this.projectId = projectId;
-  }
+    @Override
+    public boolean canProvision(Label label) {
+        return true;
+    }
 
-  public String getCredentialsId() {
-    return credentialsId;
-  }
+    @Extension
+    public static class GoogleCloudDescriptor extends Descriptor<Cloud> {
 
-  public void setCredentialsId(String credentialsId) {
-    this.credentialsId = credentialsId;
-  }
+        @Nonnull
+        @Override
+        public String getDisplayName() {
+            return Messages.ComputeEngineCloud_DisplayName();
+        }
 
+
+        public ListBoxModel doFillCredentialsIdItems(@AncestorInPath Jenkins context, @QueryParameter String source,
+                                                     @QueryParameter String value) {
+            if (context == null || !context.hasPermission(Item.CONFIGURE)) {
+                return new StandardListBoxModel();
+            }
+
+            List<DomainRequirement> domainRequirements = new ArrayList<DomainRequirement>();
+            return new StandardListBoxModel()
+                    .withEmptySelection()
+                    .withMatching(
+                            CredentialsMatchers.instanceOf(GoogleOAuth2Credentials.class),
+                            CredentialsProvider.lookupCredentials(
+                                    StandardCredentials.class, context, ACL.SYSTEM,
+                                    domainRequirements));
+        }
+
+        public FormValidation doCheckCredentialsId(@AncestorInPath Jenkins context, @QueryParameter String source,
+                                                   @QueryParameter String value) {
+            try {
+                List<DomainRequirement> domainRequirements = new ArrayList<DomainRequirement>();
+                ClientFactory clientFactory = new ClientFactory(context, domainRequirements, value);
+                return FormValidation.ok();
+            } catch (IOException ioe) {
+                return FormValidation.error(ioe.getMessage());
+            }
+        }
+    }
+
+    public String getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(String projectId) {
+        this.projectId = projectId;
+    }
+
+    public String getCredentialsId() {
+        return credentialsId;
+    }
+
+    public void setCredentialsId(String credentialsId) {
+        this.credentialsId = credentialsId;
+    }
 }
---
+++ src/main/java/com/google/jenkins/plugins/computeengine/client/ClientFactory.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.jenkins.plugins.computeengine.client;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.List;
+
+import com.cloudbees.plugins.credentials.CredentialsMatchers;
+import com.cloudbees.plugins.credentials.CredentialsProvider;
+import com.cloudbees.plugins.credentials.domains.DomainRequirement;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.jenkins.plugins.computeengine.ComputeEngineScopeRequirement;
+import com.google.jenkins.plugins.credentials.oauth.GoogleOAuth2Credentials;
+import com.google.api.services.compute.Compute;
+import hudson.AbortException;
+import hudson.model.ItemGroup;
+import hudson.security.ACL;
+
+/** Creates clients for communicating with Google APIs. */
+public class ClientFactory {
+    public static final String APPLICATION_NAME = "jenkins-google-compute-plugin";
+
+    private static HttpTransport DEFAULT_TRANSPORT;
+
+    private final ItemGroup itemGroup;
+    private final HttpTransport transport;
+    private final JsonFactory jsonFactory;
+    private final GoogleOAuth2Credentials credentials;
+    private final HttpRequestInitializer gcred;
+
+    public ClientFactory(ItemGroup itemGroup, List<DomainRequirement> domainRequirements, String credentialsId)
+            throws IOException {
+        if (credentialsId == null) {
+            throw new IllegalArgumentException(Messages.ClientFactory_CredentialsIdRequired());
+        }
+        this.itemGroup = itemGroup;
+
+        try {
+            this.transport = getDefaultTransport();
+        } catch (GeneralSecurityException e) {
+            throw new AbortException(
+                    Messages.ClientFactory_FailedToInitializeHTTPTransport(e.getMessage()));
+        }
+        this.jsonFactory = new JacksonFactory();
+
+        ComputeEngineScopeRequirement requirement = new ComputeEngineScopeRequirement();
+
+        this.credentials = CredentialsMatchers.firstOrNull(
+                CredentialsProvider.lookupCredentials(
+                        GoogleOAuth2Credentials.class,
+                        itemGroup,
+                        ACL.SYSTEM,
+                        domainRequirements),
+                CredentialsMatchers.withId(credentialsId)
+        );
+
+        if (credentials == null) {
+            throw new AbortException(Messages.ClientFactory_FailedToRetrieveCredentials(credentialsId));
+        }
+
+        try {
+            this.gcred = credentials.getGoogleCredential(requirement);
+        }catch (GeneralSecurityException e) {
+            throw new AbortException(
+                    Messages.ClientFactory_FailedToInitializeHTTPTransport(e.getMessage()));
+        }
+    }
+
+    private static synchronized HttpTransport getDefaultTransport()
+            throws GeneralSecurityException, IOException {
+        if (DEFAULT_TRANSPORT == null) {
+            DEFAULT_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        }
+        return DEFAULT_TRANSPORT;
+    }
+
+    public static synchronized void setDefaultTransport(HttpTransport transport) {
+        DEFAULT_TRANSPORT = transport;
+    }
+}
