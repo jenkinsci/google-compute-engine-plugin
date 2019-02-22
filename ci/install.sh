bc3e0de60b95271da6a563d622ca96fc8f25d897
Evan Brown <evanbrown@google.com>
2019-02-21 20:21:45 -0800

    ci: plumb artifactory config into release task

--- ci/install.sh
+++ ci/install.sh
@@ -12,4 +12,6 @@ fly -t jenkins set-pipeline \
   -v release_branch=$BRANCH \
   -v ci_branch=ci \
   -v repo_uri=https://github.com/jenkinsci/google-compute-engine-plugin.git \
+  -v artifactory_username=$ARTIFACTORY_USERNAME \
+  -v artifactory_password=$ARTIFACTORY_PASSWORD \
   -p compute-engine-plugin-$BRANCH
--- ci/tasks/release.sh
+++ ci/tasks/release.sh
@@ -4,6 +4,23 @@ set -e -u -x
 
 version=$(cat version/version)
 
+# Write a Maven config file that allows uploading the build artifact
+# to the Jenkins repository.
+cat >$HOME/.m2/settings.xml <<EOL
+<settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+  xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0
+                      http://maven.apache.org/xsd/settings-1.0.0.xsd">
+  <servers>
+    <server>
+      <id>maven.jenkins-ci.org</id> <!-- For parent 1.397 or newer; this ID is used for historical reasons and independent of the actual host name -->
+      <username>${artifactory_username}</username>
+      <password>${artifactory_password}</password>
+    </server>
+  </servers>
+</settings>
+EOL
+
 pushd plugin
   GOOGLE_PROJECT_ID=$project_id GOOGLE_CREDENTIALS=$service_account_json \
     mvn \
@@ -12,5 +29,6 @@ pushd plugin
     -DdryRun=true \
     -DreleaseVersion=$version \
     -DdevelopmentVersion=$version \
-    release:prepare
+    release:prepare \
+    release:perform
 popd
