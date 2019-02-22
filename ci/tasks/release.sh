5145230220cd48ca032c875b6cd4e1e90507e2a8
Evan Brown <evanbrown@google.com>
2019-02-21 20:03:32 -0800

    ci: batch mode for maven release

--- ci/tasks/release.sh
+++ ci/tasks/release.sh
@@ -3,11 +3,12 @@
 set -e -u -x
 
 version=$(cat version/version)
-echo $version
 
 pushd plugin
   GOOGLE_PROJECT_ID=$project_id GOOGLE_CREDENTIALS=$service_account_json \
     mvn \
+    -B \
+    -Dtag=google-compute-engine-$version \
     -DdryRun=true \
     -DreleaseVersion=$version \
     -DdevelopmentVersion=$version \
