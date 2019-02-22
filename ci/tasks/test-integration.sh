171a28daee6e15106b0570b7d4bcabb949369e46
Evan Brown <evanbrown@google.com>
2019-02-21 17:30:28 -0800

    ci: run Failsafe (int) tests without Surefire (unit) tests

--- ci/tasks/test-integration.sh
+++ ci/tasks/test-integration.sh
@@ -3,5 +3,5 @@
 set -e -u -x
 
 pushd plugin
-  GOOGLE_PROJECT_ID=$project_id GOOGLE_CREDENTIALS=$service_account_json mvn verify
+  GOOGLE_PROJECT_ID=$project_id GOOGLE_CREDENTIALS=$service_account_json mvn clean test-compile failsafe:integration-test
 popd
