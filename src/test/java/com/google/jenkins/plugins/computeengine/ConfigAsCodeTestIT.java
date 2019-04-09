6af8cd4758d0cd04379a85a2698af200337fe4cb
Karol Lassak <ingwar@ingwar.eu.org>
2019-04-10 00:23:22 +0200

    Working version of IT tests

--- src/test/java/com/google/jenkins/plugins/computeengine/ConfigAsCodeTestIT.java
+++ src/test/java/com/google/jenkins/plugins/computeengine/ConfigAsCodeTestIT.java
@@ -4,7 +4,13 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 
+import com.cloudbees.plugins.credentials.Credentials;
+import com.cloudbees.plugins.credentials.CredentialsStore;
+import com.cloudbees.plugins.credentials.SystemCredentialsProvider;
+import com.cloudbees.plugins.credentials.domains.Domain;
 import com.google.api.services.compute.model.Instance;
+import com.google.jenkins.plugins.credentials.oauth.GoogleRobotPrivateKeyCredentials;
+import com.google.jenkins.plugins.credentials.oauth.ServiceAccountConfig;
 import hudson.model.labels.LabelAtom;
 import hudson.slaves.NodeProvisioner;
 import io.jenkins.plugins.casc.misc.ConfiguredWithCode;
@@ -14,13 +20,16 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
 
-public class ConfigAsCodetestIT extends ComputeEngineCloudIT {
+public class ConfigAsCodeTestIT extends ComputeEngineCloudIT {
 
   @Rule public JenkinsRule r = new JenkinsConfiguredWithCodeRule();
 
   @Test(timeout = 300000)
   @ConfiguredWithCode("configuration-as-code-it.yml")
   public void testWorkerCreated() throws Exception {
+    // This method should be moved to some common class
+    addCredentials();
+
     ComputeEngineCloud cloud = (ComputeEngineCloud) r.jenkins.clouds.getByName("gce-integration");
     // Add a new node
     Collection<NodeProvisioner.PlannedNode> planned = cloud.provision(new LabelAtom(LABEL), 1);
@@ -39,5 +48,20 @@ public class ConfigAsCodetestIT extends ComputeEngineCloudIT {
     Instance instance = cloud.getClient().getInstance(projectId, ZONE, name);
 
     assertNotNull(logs(), instance);
+  }
+
+  private void addCredentials() throws Exception {
+    // Add a service account credential
+    projectId = System.getenv("GOOGLE_PROJECT_ID");
+    assertNotNull("GOOGLE_PROJECT_ID env var must be set", projectId);
+
+    String serviceAccountKeyJson = System.getenv("GOOGLE_CREDENTIALS");
+    assertNotNull("GOOGLE_CREDENTIALS env var must be set", serviceAccountKeyJson);
+
+    ServiceAccountConfig sac = new StringJsonServiceAccountConfig(serviceAccountKeyJson);
+    Credentials c = (Credentials) new GoogleRobotPrivateKeyCredentials(projectId, sac, null);
+
+    CredentialsStore store = new SystemCredentialsProvider.ProviderImpl().getStore(r.jenkins);
+    store.addCredentials(Domain.global(), c);
   }
 }
