69c58d6d3a231c72b2062fcb3bbb7a9ff91fdb1a
Zbynek Konecny <zbynek1729@gmail.com>
2024-10-27 13:37:06 +0100

    Use unique ID in forms

--- src/main/resources/com/google/jenkins/plugins/computeengine/ComputeEngineCloud/provision.js
+++ src/main/resources/com/google/jenkins/plugins/computeengine/ComputeEngineCloud/provision.js
@@ -1,6 +1,6 @@
 Behaviour.specify("[data-type='gcloud-provision']", 'gcloud-provision', -99, function(e) {
   e.addEventListener("click", function (event) {
-    const form = document.querySelector("form[name='provision']");
+    const form = document.getElementById(e.dataset.form);
     form.querySelector("[name='configuration']").value = e.dataset.url;
     buildFormTree(form);
     form.submit();
