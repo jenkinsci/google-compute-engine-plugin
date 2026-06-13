4800a9ae20bdb8541cc67c33add0b98c37ab3aa5
Guruprasad Bhat <gbhat@cloudbees.com>
2026-06-13 12:08:38 +0530

    spotbugs reason

--- src/main/java/com/google/jenkins/plugins/computeengine/InstanceConfiguration.java
+++ src/main/java/com/google/jenkins/plugins/computeengine/InstanceConfiguration.java
@@ -193,6 +193,9 @@ public class InstanceConfiguration implements Describable<InstanceConfiguration>
 
     /** Set by ComputeEngineCloudFallbackZoneIT only to simulate zone capacity exhaustion on the first attempt. */
     @VisibleForTesting
+    @SuppressFBWarnings(
+            value = "MS_SHOULD_BE_FINAL",
+            justification = "intentionally mutable for integration test")
     public static boolean simulateCapacityExhaustionOnFirstAttempt = false;
 
     // Optional not possible due to serialization requirement
