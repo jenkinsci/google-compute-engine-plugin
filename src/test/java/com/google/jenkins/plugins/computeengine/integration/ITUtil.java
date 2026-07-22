4fd3a9cf268610797a9a56adcba3c8c31d23822b
Guruprasad Bhat <gbhat@cloudbees.com>
2026-07-22 16:51:55 +0530

    Update src/test/java/com/google/jenkins/plugins/computeengine/integration/ITUtil.java
    Co-authored-by: James Nord <jtnord@users.noreply.github.com>

--- src/test/java/com/google/jenkins/plugins/computeengine/integration/ITUtil.java
+++ src/test/java/com/google/jenkins/plugins/computeengine/integration/ITUtil.java
@@ -124,8 +124,7 @@ class ITUtil {
     static final int SNAPSHOT_TIMEOUT = windows ? 600 : 300;
     private static final GoogleKeyCredential SSH_KEY = GoogleKeyPair.generate(RUN_AS_USER);
     static final String SSH_PRIVATE_KEY = Secret.toString(SSH_KEY.getPrivateKey());
-    static final int TEST_TIMEOUT_MULTIPLIER =
-            (System.getProperty("os.name", "").toLowerCase().contains("win") || windows) ? 3 : 1;
+    static final int TEST_TIMEOUT_MULTIPLIER = isWindows() || windows ? 3 :1;
     static final String CONFIG_AS_CODE_PATH =
             windows ? "configuration-as-code-windows-it.yml" : "configuration-as-code-it.yml";
     private static String windowsPrivateKeyCredentialsId;
